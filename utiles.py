86f13c353116119bef80dc2cdda6f7e8c6b92138
Facundo Villena <58498846+fvillena4@users.noreply.github.com>
2020-05-11 23:33:00 -0300

    Delete utiles.py

--- utiles.py
+++
@@ -1,9 +0,0 @@
-def vocales(cadena):
-	print( list(filter(lambda l: l.lower() in "aeiou", cadena)))
-
-print(__name__)
-
-# if __name__ == "__main__":
-    # import sys
-    # vocales(sys.argv[1])
-
